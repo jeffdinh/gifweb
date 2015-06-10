17ffc5698cb0de25ed52afd00bc613369eb627e1
Jeff <jeff.amnesiandc@gmail.com>
2015-06-10 17:19:43 -0400

    partial test written

--- test.rb
+++ test.rb
@@ -10,4 +10,58 @@ require './server'
 
 require 'pry'
 
+class GifBotTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    GifBotWeb
+  end
+
+  # def setup
+  #   Item.delete_all
+  #   User.delete_all
+  #   List.delete_all
+  # end
+
+  def test_can_add_gif
+    User.create! name: "Jeff"
+    post "/gif/add",
+      url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
+      created_at: datetime
+    assert_equal 200, last_response.status
+
+    check_gif = Gif.find_by_url "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
+    assert check_gif
+    assert_equal check_gif.id, last_response.body
+  end
+
+  def test_can_get_random_gif
+    User.create! name: "Bella"
+    post "/gif/add",
+      url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"
+      created_at: datetime
+
+      url: "http://media.giphy.com/media/sIIhZliB2McAo/giphy-facebook_s.jpg"
+      created_at: datetime
+
+      url:"http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
+      created_at: datetime
+
+    get "/gif" 
+    assert_equal 200, late_response.status
+
+    
+  end
+
+  def test_can_list_all
+    
+  end
+
+  def test_can_tag_a_gif
+    
+  end
+
+  def test_can_list_gifs_specified_tag
+    
+  end
 end
