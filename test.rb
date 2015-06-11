eecdcce59515c1800308374219236b32444b41b3
Bella <woo.bella@gmail.com>
2015-06-11 13:43:38 -0400

    notes from morning lecture applicable to tests

--- test.rb
+++ test.rb
@@ -27,7 +27,7 @@ class GifBotTest < Minitest::Test
     User.create! name: "Jeff"
     post "/gif/add",
       url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
-      created_at: datetime
+      # created_at: datetime
     assert_equal 200, last_response.status
 
     check_gif = Gif.find_by_url "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
@@ -38,14 +38,14 @@ class GifBotTest < Minitest::Test
   def test_can_get_random_gif
     User.create! name: "Bella"
     post "/gif/add",
-      url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"
-      created_at: datetime
+      url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif",
+      # created_at: datetime
 
-      url: "http://media.giphy.com/media/sIIhZliB2McAo/giphy-facebook_s.jpg"
-      created_at: datetime
+      url: "http://media.giphy.com/media/sIIhZliB2McAo/giphy-facebook_s.jpg",
+      # created_at: datetime
 
       url:"http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
-      created_at: datetime
+      # created_at: datetime
 
     get "/gif" 
     assert_equal 200, late_response.status
