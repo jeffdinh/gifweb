937f357779e41b71af497b9783dcc21b47da6253
Jeff <jeff.amnesiandc@gmail.com>
2015-06-11 14:17:22 -0400

    updated tests w/ working syntax

--- test.rb
+++ test.rb
@@ -26,8 +26,9 @@ class GifBotTest < Minitest::Test
   def test_can_add_gif
     User.create! name: "Jeff"
     post "/gif/add",
-      url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
-      created_at: datetime
+      url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif",
+      created_at: Time.now
+      
     assert_equal 200, last_response.status
 
     check_gif = Gif.find_by_url "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
@@ -37,28 +38,32 @@ class GifBotTest < Minitest::Test
 
   def test_can_get_random_gif
     User.create! name: "Bella"
-    post "/gif/add",
-      url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"
-      created_at: datetime
+      post "/gif/add",
+      url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif",
+      created_at: Time.now
 
-      url: "http://media.giphy.com/media/sIIhZliB2McAo/giphy-facebook_s.jpg"
-      created_at: datetime
-
-      url:"http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
-      created_at: datetime
-
-    get "/gif" 
-    assert_equal 200, late_response.status
+      post "/gif/add",
+      url: "http://media0.gphy.com/media/jUwpNzg9IcyrK/giphy.gif",
+      created_at: Time.now
+      
+      post "/gif/add",
+      url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif",
+      created_at: Time.now
+      
+      get "/gif" 
+      assert_equal 200, late_response.status
   end
 
   def test_can_store_times_gif_seen
     User.create! name: "Jack"
-    post "/gif/add",
-      url: "http://media0.giphy.com/media/jUwpNzg9Icyr/giphy.gif"
-      created_at: datetime
-
-      url: "http://media.giphy.com/media/sIIhZliB2McA/giphy-facebook_s.jpg"
-      created_at: datetime
+     post "/gif/add",
+      url: "http://media0.giphy.com/media/jUwpNzg9Icyr/giphy.gif",
+      created_at: Time.now
+      
+      post "/gif/add",
+      url: "http://media.giphy.com/media/sIIhZliB2McA/giphy-facebook_s.jpg",
+      created_at: Time.now
+      
 
       get "gif/2"
       gif = Gif.find_by_id 2 
@@ -66,6 +71,17 @@ class GifBotTest < Minitest::Test
   end
 
   def test_can_list_all
+    User.create! name: "Mike"
+     post "/gif/add",
+      url: "http://media0.giphy.com/meda/jUwpNzg9Icyr/giphy.gif",
+      created_at: Time.now
+
+      post "/gif/add",
+      url: "http://media0.giphy.com/meda/jUwpNzg9Icyr/giphy.gif",
+      created_at: Time.now
+      
+      get "gif/all",
+
     
   end
 
