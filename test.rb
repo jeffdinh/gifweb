9e21ef7f90b5b2a1568daa2023e9b82daa6b333d
Bella <woo.bella@gmail.com>
2015-06-13 22:50:14 -0400

    all tests written but with JSON errors

--- test.rb
+++ test.rb
@@ -24,8 +24,9 @@ class GifBotTest < Minitest::Test
   end
 
   def test_can_add_gif
-    User.create! name: "Jeff"
+    j = User.create! name: "Jeff"
     post "/gif/add",
+    	username: j.name,
       url: "http://media2.giphy.com/media/8KrhxtEsrdhD2/giphy.gif"
 
     assert_equal 200, last_response.status
@@ -36,36 +37,38 @@ class GifBotTest < Minitest::Test
   end
 
   def test_can_get_random_gif
-    User.create! name: "Bella"
-      post "/gif/add",
-        url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif",
-        seen_count: 0
+    b = User.create! name: "Bella"
+    b1 = b.gifs.create! url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif", seen_count: 0
+    b2 = b.gifs.create! url: "http://i.imgur.com/gustJxn.gif", seen_count: 0
+    b3 = b.gifs.create! url: "http://i.imgur.com/Hsnufqt.gif", seen_count: 0
+      # post "/gif/add",
+      #   url: "http://media0.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"
 
-      post "/gif/add",
-        url: "http://i.imgur.com/gustJxn.gif",
-        seen_count: 0
+      # post "/gif/add",
+      #   url: "http://i.imgur.com/gustJxn.gif"
       
-      post "/gif/add",
-        url: "http://i.imgur.com/Hsnufqt.gif",
-        seen_count: 0
+      # post "/gif/add",
+      #   url: "http://i.imgur.com/Hsnufqt.gif"
       
       get "/gif" 
-      assert_equal 200, last_response.status
+      random_results = JSON.parse last_response.body
 
-      random_results = Gif.first
+      assert_equal 200, last_response.status
       assert_equal 1, random_results["seen_count"]
   end
 
   def test_can_store_times_gif_seen
-  	skip
-    User.create! name: "Jack"
-     post "/gif/add",
-      url: "http://i.imgur.com/aHK7ZVX.gif"
+  	jck = User.create! name: "Jack"
+   	j = jck.gifs.create! url: "http://i.imgur.com/aHK7ZVX.gif", seen_count: 0
+   	# post "/gif/add",
+   	# 	username: jck.name,
+    # 	url: "http://i.imgur.com/aHK7ZVX.gif"
 
-     get "gif/all"
-      
-      gif = Gif.find_by_url "http://i.imgur.com/aHK7ZVX.gif"
-      assert_equal 1, gif.seen_count
+  	get "gif/all"
+		
+		looked_at_gif = JSON.parse last_response.body
+		assert_equal 200, last_response.status
+		assert_equal 1, looked_at_gif["seen_count"]
   end
 
   def test_can_list_all
@@ -89,18 +92,40 @@ class GifBotTest < Minitest::Test
 
   def test_can_tag_a_gif
     User.create! name: "Kayla"
-     to_tag = post "/gif/add",
-      url: "http://i.imgur.com/OH3IkBS.gif"
+		to_tag = post "/gif/add",
+			url: "http://i.imgur.com/OH3IkBS.gif"
 
-     patch "/gif/tag",
-     	id: to_tag.id,
-      tag: "Funny"
+		patch "/gif/tag",
+			id: to_tag.id,
+			tag_name: "Funny"
 
-     assert_equal 200, last_response.status
-     assert_equal true, gif.url.include?("http://i.imgur.com/OH3IkBS.gif")
+		assert_equal 200, last_response.status
+		assert_equal true, gif.url.include?("http://i.imgur.com/OH3IkBS.gif")
   end
 
   def test_can_list_gifs_specified_tag
-    skip
+    s = User.create! name: "Saka"
+    s1 = s.gifs.create! url: "http://i.imgur.com/4RdyePJ.gif", seen_count: 0
+    s2 = s.gifs.create! url: "http://i.imgur.com/rbO4s5N.gif", seen_count: 0
+    s3 = s.gifs.create! url: "http://i.imgur.com/ORLGZeC.gif", seen_count: 0
+
+    patch "/gif/tag",
+    	id: s1.id,
+    	tag_name: "Dancing"
+
+    patch "/gif/tag",
+    	id: s2.id,
+    	tag_name: "Fran"
+
+  	patch "/gif/tag",
+  		id: s3.id,
+  		tag_name: "Fran"
+
+  	get "/gif/Fran"
+
+  	frans = JSON.parse last_response.body
+
+  	assert_equal 200, last_response.status
+  	assert_equal 2, frans.count
   end
 end
